aa8138b5abf70166f84de5a9c80c7d1fee4d72f4
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-05-03 14:14:09 +0100

    Removed AWS creds handling, to test IAM role assignment

--- start.sh
+++ start.sh
@@ -7,8 +7,6 @@ USER_UID=1001
 useradd -g $GROUP -u $USER_UID -d /home/$USER $USER
 
 echo "auth-proxy-sign-in-url=https://${USER}-rstudio.${TOOLS_DOMAIN}/logout" >> /etc/rstudio/rserver.conf
-echo "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" >> /usr/local/lib/R/etc/Renviron
-echo "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" >> /usr/local/lib/R/etc/Renviron
 echo "AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}" >> /usr/local/lib/R/etc/Renviron
 
 /usr/lib/rstudio-server/bin/rserver --server-daemonize 0
